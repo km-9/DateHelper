6da019a5804bb706737f9e96c8eb5eed07d9a67c
km-9 <kylemonteleone96@gmail.com>
2022-11-04 11:57:20 -0400

    basic setup /w springboot

---
+++ src/test/java/com/fof/DateHelper/DateHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fof.DateHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
